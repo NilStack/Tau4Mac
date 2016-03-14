621d391c8428aa82c5af97df33cc3dd9b7f60c2f
Tong Kuo <contact@tong-kuo.me>
2016-03-14 19:30:22 +0800

    Introduced Unit Test for Tau4Mac

--- Tau4MacTests/Tau4MacTests.m
+++ Tau4MacTests/Tau4MacTests.m
@@ -6,34 +6,42 @@
 //  Copyright © 2016 Tong Kuo. All rights reserved.
 //
 
-@import XCTest;
+#import "TauAbstractResultCollection.h"
 
+// Tau4MacTests class
 @interface Tau4MacTests : XCTestCase
 
-@end
+@end // Tau4MacTests class
 
+// Tau4MacTests class
 @implementation Tau4MacTests
 
-- (void)setUp {
-    [super setUp];
+- ( void ) setUp
+    {
+    [ super setUp ];
     // Put setup code here. This method is called before the invocation of each test method in the class.
-}
+    }
 
-- (void)tearDown {
+- ( void ) tearDown
+    {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
+    [ super tearDown ];
+    }
 
-- (void)testExample {
+- ( void ) testExample
+    {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
+    }
 
-- (void)testPerformanceExample {
+- ( void ) testPerformanceExample
+    {
     // This is an example of a performance test case.
-    [self measureBlock:^{
+    [ self measureBlock:
+    ^( void )
+        {
         // Put the code you want to measure the time of here.
-    }];
-}
+        } ];
+    }
 
-@end
+@end // Tau4MacTests class
