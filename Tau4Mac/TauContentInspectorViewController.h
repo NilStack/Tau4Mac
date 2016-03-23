f3f38a2178dc29c38b8d537558e8d78cb5f078a9
Tong Kuo <contact@tong-kuo.me>
2016-03-23 15:16:37 +0800

    Introduced the TauContentInspectorViewController class

---
+++ Tau4Mac/TauContentInspectorViewController.h
@@ -0,0 +1,25 @@
+//
+//  TauContentInspectorViewController.h
+//  Tau4Mac
+//
+//  Created by Tong G. on 3/23/16.
+//  Copyright © 2016 Tong Kuo. All rights reserved.
+//
+
+@class TauContentInspectorView;
+
+// TauContentInspectorViewController class
+@interface TauContentInspectorViewController : NSViewController
+
+@end // TauContentInspectorViewController class
+
+
+
+// ------------------------------------------------------------------------------------------------------------ //
+
+
+
+//TauContentInspectorView class
+@interface TauContentInspectorView : NSView
+
+@end //TauContentInspectorView class
---
+++ Tau4Mac/TauContentInspectorViewController.m
@@ -0,0 +1,42 @@
+//
+//  TauContentInspectorViewController.m
+//  Tau4Mac
+//
+//  Created by Tong G. on 3/23/16.
+//  Copyright © 2016 Tong Kuo. All rights reserved.
+//
+
+#import "TauContentInspectorViewController.h"
+
+// Private
+@interface TauContentInspectorViewController ()
+
+@end // Private
+
+
+
+// ------------------------------------------------------------------------------------------------------------ //
+
+
+
+// TauContentInspectorViewController class
+@implementation TauContentInspectorViewController
+
+- ( void ) viewDidLoad
+    {
+    [ super viewDidLoad ];
+    // Do view setup here.
+    }
+
+@end // TauContentInspectorViewController class
+
+
+
+// ------------------------------------------------------------------------------------------------------------ //
+
+
+
+// TauContentInspectorView class
+@implementation TauContentInspectorView
+
+@end // TauContentInspectorView class
