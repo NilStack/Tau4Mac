522782c9629b81493b45afcb8492d4fae2cbc3f9
Tong Kuo <contact@tong-kuo.me>
2016-03-25 13:14:55 +0800

    Cleaned up

--- Tau4Mac/TauContentInspectorViewController.m
+++ Tau4Mac/TauContentInspectorViewController.m
@@ -7,7 +7,6 @@
 //
 
 #import "TauContentInspectorViewController.h"
-#import "TauAbstractDisclosureViewController.h"
 
 
 
@@ -28,10 +27,6 @@
 
 // These guys used for feeding the self.stackView_
 @property ( weak ) IBOutlet NSView* noSelectionLabelSection_;
-@property ( weak ) IBOutlet TauAbstractDisclosureViewController* singleContentTitleDisclosureViewController_;
-@property ( weak ) IBOutlet TauAbstractDisclosureViewController* singleContentDescriptionDisclosureViewController_;
-@property ( weak ) IBOutlet TauAbstractDisclosureViewController* singleContentActionDisclosureViewController_;
-@property ( weak ) IBOutlet TauAbstractDisclosureViewController* singleContentMetaInfoDisclosureViewController_;
 
 @property ( weak ) IBOutlet NSArrayController* ytContentModelController_;
 
@@ -56,8 +51,6 @@
     {
     [ super viewDidLoad ];
     // Do view setup here.
-
-    NSLog( @"%@", self.stackView_ );
     }
 
 - ( void ) dealloc
@@ -96,36 +89,6 @@
 
 #pragma mark - Private
 
-@synthesize stackView_ = priStackView_;
-- ( NSStackView* ) stackView_
-    {
-    if ( !priStackView_ )
-        {
-        priStackView_ = [ [ NSStackView alloc ] initWithFrame: NSZeroRect ];
-
-        [ priStackView_ addView: self.singleContentTitleDisclosureViewController_.view inGravity: NSStackViewGravityTop ];
-        [ priStackView_ addView: self.singleContentActionDisclosureViewController_.view inGravity: NSStackViewGravityTop ];
-        [ priStackView_ addView: self.singleContentDescriptionDisclosureViewController_.view inGravity: NSStackViewGravityCenter ];
-        [ priStackView_ addView: self.singleContentMetaInfoDisclosureViewController_.view inGravity: NSStackViewGravityBottom ];
-
-        [ priStackView_ setVisibilityPriority: NSStackViewVisibilityPriorityMustHold forView: self.singleContentTitleDisclosureViewController_.view ];
-        [ priStackView_ setVisibilityPriority: NSStackViewVisibilityPriorityMustHold forView: self.singleContentActionDisclosureViewController_.view ];
-        [ priStackView_ setVisibilityPriority: NSStackViewVisibilityPriorityDetachOnlyIfNecessary forView: self.singleContentDescriptionDisclosureViewController_.view ];
-        [ priStackView_ setVisibilityPriority: NSStackViewVisibilityPriorityDetachOnlyIfNecessary - 1 forView: self.singleContentMetaInfoDisclosureViewController_.view ];
-
-        priStackView_.orientation = NSUserInterfaceLayoutOrientationVertical;
-        priStackView_.alignment = NSLayoutAttributeCenterX;
-        priStackView_.spacing = 0.f;
-
-        [ priStackView_ setHuggingPriority: NSLayoutPriorityDefaultHigh forOrientation: NSLayoutConstraintOrientationHorizontal ];
-
-        [ self.view addSubview: [ priStackView_ configureForAutoLayout ] ];
-        [ priStackView_ autoPinEdgesToSuperviewEdges ];
-        }
-
-    return priStackView_;
-    }
-
 @end // TauContentInspectorViewController class
 
 
@@ -140,24 +103,7 @@
 - ( void ) awakeFromNib
     {
     [ [ self configureForAutoLayout ] setWantsLayer: YES ];
-    [ self.layer setBackgroundColor: [ NSColor whiteColor ].CGColor ];    }
-
-- ( void ) drawRect: ( NSRect )_DirtyRect
-    {
-    [ super drawRect: _DirtyRect ];
-
-    NSBezierPath* cuttingLinePath = [ NSBezierPath bezierPath ];
-
-    NSRect bounds = self.bounds;
-    CGFloat y = self.isFlipped ? NSMinY( bounds ) : NSMaxY( bounds );
-    [ cuttingLinePath moveToPoint: NSMakePoint( NSMinX( bounds ), y ) ];
-    [ cuttingLinePath lineToPoint: NSMakePoint( NSMaxX( bounds ), y ) ];
-
-    cuttingLinePath.lineWidth = 1.5f;
-
-    [ [ [ NSColor lightGrayColor ] colorWithAlphaComponent: .6f ] setStroke ];
-    [ cuttingLinePath stroke ];
-//    [ cuttingLinePath fill ];
+    [ self.layer setBackgroundColor: [ NSColor whiteColor ].CGColor ];
     }
 
 @end // TauContentInspectorSectionView class
