b5bb6dd9a3891e695f5d3ad4b69b5eada0edc499
Tong Kuo <contact@tong-kuo.me>
2016-04-02 22:04:24 +0800

    TauSearchQuery is a stupid and thin layer

--- Tau4Mac/TauSearchDashboardController.h
+++ Tau4Mac/TauSearchDashboardController.h
@@ -6,6 +6,45 @@
 //  Copyright © 2016 Tong Kuo. All rights reserved.
 //
 
+typedef NS_ENUM ( NSInteger, TauSearchContentOrder )
+    { TauSearchContentDateOrder = 0
+    , TauSearchContentRatingOrder = 1
+    , TauSearchContentRelevanceOrder = 2
+    , TauSearchContentTitleOrder = 3
+    , TauSearchContentVideoCountOrder = 4
+    , TauSearchContentViewCountOrder = 5
+    };
+
+typedef NS_ENUM ( NSUInteger, TauSearchVideoDefinition )
+    { TauSearchVideoAnyDefinition
+    , TauSearchVideoHighDefinition
+    , TauSearchVideoStandardDefinition
+    };
+
+typedef NS_ENUM ( NSUInteger, TauSearchVideoDuration )
+    { TauSearchVideoAnyDuration
+    , TauSearchVideoLongDuration
+    , TauSearchVideoMediumDuration
+    , TauSearchVideoShortDuration
+    };
+
+typedef NS_ENUM ( NSUInteger, TauSearchVideoType )
+    { TauSearchVideoAnyType
+    , TauSearchVideoEpisodeType
+    , TauSearchVideoMovieType
+    };
+
+typedef NS_ENUM ( NSUInteger, TauSearchVideoLicense )
+    { TauSearchVideoAnyLicense
+    , TauSearchVideoCreativeCommonsLicense
+    , TauSearchVideoStandardYouTubeLicense
+    };
+
+typedef NS_ENUM ( NSUInteger, TauSearchChannelType )
+    { TauSearchChannelAnyType
+    , TauSearchChannelOnlyShowType
+    };
+
 // TauSearchDashboardController class
 @interface TauSearchDashboardController : NSViewController
 
@@ -32,5 +71,7 @@
 #pragma mark - Specific to YouTune Channels
 
 @property ( weak ) IBOutlet NSSegmentedControl* channelTypeSegControl;
+
+@property ( copy, readonly ) GTLQueryYouTube* YouTubeQuery;
 
 @end // TauSearchDashboardController class
--- Tau4Mac/TauSearchDashboardController.m
+++ Tau4Mac/TauSearchDashboardController.m
@@ -37,4 +37,113 @@
     // Do view setup here.
     }
 
+@dynamic YouTubeQuery;
+- ( GTLQueryYouTube* ) YouTubeQuery
+    {
+    GTLQueryYouTube* query = [ GTLQueryYouTube queryForSearchListWithPart: @"snippet" ];
+
+    // Converting contentTypes field
+    {
+    NSMutableArray* types = [ @[] mutableCopy ];
+    if ( [ searchVideoCheckBox state ] == NSOnState ) [ types addObject: @"video" ];
+    if ( [ searchPlaylistCheckBox state ] == NSOnState ) [ types addObject: @"playlist" ];
+    if ( [ searchChannelCheckBox state ] == NSOnState ) [ types addObject: @"channel" ];
+
+    query.type = [ types componentsJoinedByString: @" " ];
+    }
+
+    // Converting order field
+    {
+    NSString* orderString = nil;
+    switch ( searchOrderPopUp.selectedTag )
+        {
+        case TauSearchContentDateOrder: orderString = @"date"; break;
+        case TauSearchContentRatingOrder: orderString = @"rating"; break;
+        case TauSearchContentRelevanceOrder: orderString = @"relevance"; break;
+        case TauSearchContentTitleOrder: orderString = @"title"; break;
+        case TauSearchContentVideoCountOrder: orderString = @"videoCount"; break;
+        case TauSearchContentViewCountOrder: orderString = @"viewCount"; break;
+        }
+
+    query.order = orderString;
+    }
+
+    {
+    query.publishedAfter = [ GTLDateTime dateTimeWithDate: publishedAfterDatePicker.dateValue timeZone: [ NSTimeZone localTimeZone ] ];
+    query.publishedBefore = [ GTLDateTime dateTimeWithDate: publishedBeforeDatePicker.dateValue timeZone: [ NSTimeZone localTimeZone ] ];
+    query.regionCode = [ [ [ regionCodesPopUp selectedItem ] title ] uppercaseString ];
+    query.relevanceLanguage = [ [ [ languageCodesPopUp selectedItem ] title ] uppercaseString ];
+    }
+
+    // Converting videoDefinition field
+    {
+    NSString* videoDefString = nil;
+    switch ( videoDefinitionSegControl.selectedSegment )
+        {
+        case TauSearchVideoAnyDefinition: videoDefString = @"any"; break;
+        case TauSearchVideoHighDefinition: videoDefString = @"high"; break;
+        case TauSearchVideoStandardDefinition: videoDefString = @"standard"; break;
+        }
+
+    query.videoDefinition = videoDefString;
+    }
+
+    // Converting videoDuration field
+    {
+    NSString* durationString = nil;
+    switch ( videoDurationSegControl.selectedSegment )
+        {
+        case TauSearchVideoAnyDuration: durationString = @"any"; break;
+        case TauSearchVideoLongDuration: durationString = @"long"; break;
+        case TauSearchVideoMediumDuration: durationString = @"medium"; break;
+        case TauSearchVideoShortDuration: durationString = @"short"; break;
+        }
+
+    query.videoDuration = durationString;
+    }
+
+    // Converting videoType field
+    {
+    NSString* videoTypeString = nil;
+    switch ( videoTypeSegControl.selectedSegment )
+        {
+        case TauSearchVideoAnyType: videoTypeString = @"any"; break;
+        case TauSearchVideoEpisodeType: videoTypeString = @"episode"; break;
+        case TauSearchVideoMovieType: videoTypeString = @"movie"; break;
+        }
+
+    query.videoType = videoTypeString;
+    }
+
+    // Converting videoLicense field
+    {
+    NSString* license = nil;
+    switch ( videoLicensePopUp.selectedTag )
+        {
+        case TauSearchVideoAnyLicense: license = @"any"; break;
+        case TauSearchVideoCreativeCommonsLicense: license = @"creativeCommon"; break;
+        case TauSearchVideoStandardYouTubeLicense: license = @"youtube"; break;
+        }
+
+    query.videoLicense = license;
+    }
+
+    // Converting syndicatedVideo field
+    query.videoSyndicated = syndicatedVideoCheckBox.state ? @"true" : nil;
+
+    // Converting channelType field
+    {
+    NSString* channelTypeString = nil;
+    switch ( channelTypeSegControl.selectedSegment )
+        {
+        case TauSearchChannelAnyType: channelTypeString = @"any"; break;
+        case TauSearchChannelOnlyShowType: channelTypeString = @"show"; break;
+        }
+
+    query.channelType = channelTypeString;
+    }
+
+    return query;
+    }
+
 @end // TauSearchDashboardController class
