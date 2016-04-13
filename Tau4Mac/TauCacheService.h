3d31f0dd6fd914950e2ea3135720cb550f978e46
Tong Kuo <contact@tong-kuo.me>
2016-04-13 10:15:36 +0800

    Introduced TauCacheService class

---
+++ Tau4Mac/TauCacheService.h
@@ -0,0 +1,12 @@
+//
+//  TauCacheService.h
+//  Tau4Mac
+//
+//  Created by Tong G. on 4/13/16.
+//  Copyright © 2016 Tong Kuo. All rights reserved.
+//
+
+// TauCacheService class
+@interface TauCacheService : NSObject
+
+@end // TauCacheService class
