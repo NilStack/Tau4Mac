a45edd7f14f27de5f70e66de404359b220d3da00
Tong Kuo <contact@tong-kuo.me>
2016-04-13 10:28:59 +0800

    Renamed TauCacheService with a more suitable name

---
+++ Tau4Mac/TauArchiveService.h
@@ -0,0 +1,12 @@
+//
+//  TauArchiveService.h
+//  Tau4Mac
+//
+//  Created by Tong G. on 4/13/16.
+//  Copyright © 2016 Tong Kuo. All rights reserved.
+//
+
+// TauArchiveService class
+@interface TauArchiveService : NSObject
+
+@end // TauArchiveService class
---
+++ Tau4Mac/TauArchiveService.m
@@ -0,0 +1,14 @@
+//
+//  TauArchiveService.m
+//  Tau4Mac
+//
+//  Created by Tong G. on 4/13/16.
+//  Copyright © 2016 Tong Kuo. All rights reserved.
+//
+
+#import "TauArchiveService.h"
+
+// TauArchiveService class
+@implementation TauArchiveService
+
+@end // TauArchiveService class
