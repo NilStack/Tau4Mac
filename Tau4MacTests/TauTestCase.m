b1f96fef4100ccec69321fb7dd9f02c4ddd6f020
Tong Kuo <contact@tong-kuo.me>
2016-05-19 16:59:29 +0800

    Implemented the identifier samples of TauTestCase class

--- Tau4MacTests/TauTestCase.m
+++ Tau4MacTests/TauTestCase.m
@@ -17,9 +17,7 @@
     {
     [ super setUp ];
 
-
-    /* 
-    Valid Channel identifiers:
+    /*  valid channel identifiers:
 
     UCqhnX4jA0A5paNd1v-zEysw        UClwg08ECyHnm_RzY1wnZC1A        UC15_JWyO0xsrV-W-pYdFtBQ        UCTs-d2DgyuJVRICivxe2Ktg        UCse-sH9XJlKIbtD3y-bch7w
     UCQMle4QI2zJuOI5W5TOyOcQ        UCPGBPIwECAUJON58-F2iuFA        UC7dLLOOzmcrYyNmIKBq1XjQ        UCHt_A74xM2Hs5L5otMhhQFw        UChqor0VU2yKWnCtXryIzOiw
@@ -33,7 +31,65 @@
     channelIdentifiersSample3 = @[];
     channelIdentifiersSample4 = @[ @"", @"" ];
 
+    /* valid video identifiers:
     
+    4DSNJ_WvfN4     GgutlKV42SY     eF2QhJjCaDc     NepG36wz9H8     UcoS9aQItgA
+    CoLpsD0gGik     zjrfMgFOb9Y     Jrtn5nBM3Dw     XVfOe5mFbAE     AIPsaGGgTRc
+    Bl0qM_U5kBU     3JPRjROC0SQ     mM1P41qeVGs     7BY1WccX_Is     RWxqSEMXWuw
+    v9OpAewxljc     wo-ygJTQRPw     kxLY59nH5cI     7d59O6cfaM0     rAOISmMmloo
+    */
+
+    videoIdentifiersSample0 = @[ @"4DSNJ_WvfN4", @"GgutlKV42SY", @"eF2QhJjCaDc", @"NepG36wz9H8", @"UcoS9aQItgA" ];
+    videoIdentifiersSample1 = @[ @"CoLpsD0gGik", @"zjrfMgFOb9Y", @"Jrtn5nBM3Dw", @"XVfOe5mFbAE", @"AIPsaGGgTRc" ];
+    videoIdentifiersSample2 = nil;
+    videoIdentifiersSample3 = @[];
+    videoIdentifiersSample4 = @[ @"", @"" ];
+
+    /* valid playlist item identifiers:
+    
+    UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC4xMkVGQjNCMUM1N0RFNEUx    UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC41MjE1MkI0OTQ2QzJGNzNG
+    UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC4yODlGNEE0NkRGMEEzMEQy    UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC41NkI0NEY2RDEwNTU3Q0M2
+    UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC4wOTA3OTZBNzVEMTUzOTMy    UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC41MzJCQjBCNDIyRkJDN0VD
+    UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC5DQUNERDQ2NkIzRUQxNTY1    UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC45NDk1REZENzhEMzU5MDQz
+    UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC5GNjNDRDREMDQxOThCMDQ2    UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC40NzZCMERDMjVEN0RFRThB
+    UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC5EMEEwRUY5M0RDRTU3NDJC    UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC45ODRDNTg0QjA4NkFBNkQy
+    UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC4zMDg5MkQ5MEVDMEM1NTg2    UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC41Mzk2QTAxMTkzNDk4MDhF
+    UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC5EQUE1NTFDRjcwMDg0NEMz    UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC41QTY1Q0UxMTVCODczNThE
+    UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC4yMUQyQTQzMjRDNzMyQTMy    UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC45RTgxNDRBMzUwRjQ0MDhC
+    UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC5ENDU4Q0M4RDExNzM1Mjcy    sUEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC4yMDhBMkNBNjRDMjQxQTg1
+    */
+
+    playlistItemIdentifiersSample0 =
+        @[ @"UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC4xMkVGQjNCMUM1N0RFNEUx", @"UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC41MjE1MkI0OTQ2QzJGNzNG"
+         , @"UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC4yODlGNEE0NkRGMEEzMEQy", @"UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC41NkI0NEY2RDEwNTU3Q0M2"
+         , @"UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC4wOTA3OTZBNzVEMTUzOTMy", @"UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC41MzJCQjBCNDIyRkJDN0VD"
+         , @"UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC5DQUNERDQ2NkIzRUQxNTY1", @"UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC45NDk1REZENzhEMzU5MDQz"
+         ];
+
+    playlistItemIdentifiersSample1 =
+        @[ @"UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC5GNjNDRDREMDQxOThCMDQ2", @"UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC40NzZCMERDMjVEN0RFRThB"
+         , @"UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC5EMEEwRUY5M0RDRTU3NDJC", @"UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC45ODRDNTg0QjA4NkFBNkQy"
+         , @"UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC4zMDg5MkQ5MEVDMEM1NTg2", @"UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC41Mzk2QTAxMTkzNDk4MDhF"
+         , @"UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC5EQUE1NTFDRjcwMDg0NEMz", @"UEwwbG85TU9CZXRFSEJNNFotbFk3UzNMajdpZkdRTTQyRC41QTY1Q0UxMTVCODczNThE"
+         ];
+
+    playlistItemIdentifiersSample2 = nil;
+    playlistItemIdentifiersSample3 = @[];
+    playlistItemIdentifiersSample4 = @[ @"", @"" ];
+
+    /* valid playlist identifiers:
+    
+    PL1N57mwBHtN1co-Qt5FPejAhqg-dgwOIC  PLzj7TwUeMQ3i1QJ6-Z91KRjFg5dzacD1O  PL2Qq6_3SVp4NWceaoN_VuR9ml1BQAbHxQ  PLw2HD3prgOu3OpnVqwkoAS4y9KIlqaAXW  PLzj7TwUeMQ3jkkDD_N-nRHQTp9oX7Smam
+    PLDTQLSdym9HjUHGZ1pHJDiEGbVMhM__TE  PLhKFRV3-UgpeA_3wzRHF8AS8T7ppKvm9O  PLXPr7gfUMmKyE22-YpbgcDfr2SXEO7-qX  PLPie5drlGW25NnMjef7HRdSAqEjxz66Ky  PLEC59ED056A5F1C58
+    PL2Qq6_3SVp4P4N0Wyusc_-256lXZ3DVsw  PLKyhCiyLgYYFD5Xo6SXEYvEm_fBUzsOZD  PLIqQdbkf_WOuRBAHjxf3K0OouAjOWM5pq  PL2Qq6_3SVp4MmX8WoCJsQM_LI8qQ75Get  PLsSecSBtcB6DFvb0XSPY9hLwtBRK4FX-o
+    PLYMOUCVo86jEeMMdaaq03jQ_t9nFV737s  PLA315AFCD23B50347                  PLtvjQSgFXE1ciL7S-GW-K4tCxa_RoK-ZK  PLyQFNhZCWRn8IkyqeXCNE8lOik4ueRC7j  PL890E75C547F8AABB
+    */
+
+    playlistIdentifiersSample0 = @[ @"PL1N57mwBHtN1co-Qt5FPejAhqg-dgwOIC", @"PLzj7TwUeMQ3i1QJ6-Z91KRjFg5dzacD1O", @"PL2Qq6_3SVp4NWceaoN_VuR9ml1BQAbHxQ", @"PLw2HD3prgOu3OpnVqwkoAS4y9KIlqaAXW", @"PLzj7TwUeMQ3jkkDD_N-nRHQTp9oX7Smam" ];
+    playlistIdentifiersSample1 = @[ @"PLDTQLSdym9HjUHGZ1pHJDiEGbVMhM__TE", @"PLhKFRV3-UgpeA_3wzRHF8AS8T7ppKvm9O", @"PLXPr7gfUMmKyE22-YpbgcDfr2SXEO7-qX", @"PLPie5drlGW25NnMjef7HRdSAqEjxz66Ky", @"PLEC59ED056A5F1C58" ];
+    playlistIdentifiersSample2 = nil;
+    playlistIdentifiersSample3 = @[];
+    playlistIdentifiersSample4 = @[ @"", @"" ];
     }
 
 #pragma mark - Unit Test Samples
